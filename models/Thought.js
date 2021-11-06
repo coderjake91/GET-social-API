83af5ff6524bb6564a759afc39fb52c5b80f862d
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-06 18:05:24 -0500

    added Thought model with reactionCount virtual and validators

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,35 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('dateformat');
+const dateFormat, { masks } = require('dateformat');
 
 //create the Thought schema
+
+const ThoughtSchema = new Schema({
+    thoughtText:{
+        type: String,
+        required: "Don't forget to include your thought!",
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(nowcreatedAtVal, "dddd, mmmm dS, yyyy, h:MM:ss TT")
+    },
+    username: {
+        type: String,
+        required: 'Please include a username'
+    },
+    reactions: [],
+},
+    {
+        toJSON:{
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
