595c4dc0bd67ec84977ed981c44acf325357a729
coderjake91 <jacob.b.frazer@gmail.com>
2021-11-06 19:42:35 -0500

    added inital Thought and User controller structure

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,35 @@
-const {} = require('../models');
+const { User, Thought } = require('../models');
+
+//Thought controller
+const thoughtController = {
+    //GET all thoughts
+    getAllThoughts() {
+
+    },
+    //GET a single though by _id
+    getThoughtById() {
+
+    },
+    //POST a new thought (also push the created thoughts to the associated user)
+    createThought() {
+
+    },
+    //PUT to update a thought by _id
+    updateThought() {
+
+    },
+    //DELETE to remove a though by _id
+    deleteThought() {
+
+    },
+    //POST to create a reaction (stored in a thought's reaction array field)
+    createReaction() {
+
+    },
+    //DELETE to pull and remove a reaction by the reactions reactionId value
+    deleteReaction() {
+
+    }
+};
+
+module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,36 @@
-const {} = require('../models');
+const { model } = require('mongoose');
+const { User } = require('../models');
+
+//User controller
+const userController = {
+    //GET all users
+    getAllUsers() {
+
+    },
+    //GET a single user by _id
+    getUserById() {
+
+    },
+    //POST a new user
+    createUser() {
+
+    },
+    //PUT to update a user by _id
+    updateUser() {
+
+    },
+    //DELETE to remove as user by _id
+    deleteUser() {
+
+    },
+    //POST a new friend to the user's friend list by friend _id
+    addFriendById() {
+
+    },
+    //DELETE a friend from the user's friend list by friend _id
+    deleteFriendById() {
+        
+    }
+};
+
+model.exports = userController;
